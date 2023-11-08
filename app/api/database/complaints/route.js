7691db7a72f0605e966b51d689cbd4c4764767dd
Alejandro Silva <u201815776@upc.edu.pe>
2023-11-08 05:11:31 -0500

    feat: api methods messages and complaints
    Added the api methods get and post for the messages and the complaints

---
+++ app/api/database/complaints/route.js
@@ -0,0 +1,25 @@
+import { prisma } from "@/lib/prisma";
+import { IdManager } from '@/utils/IdManager'
+
+export async function GET() {
+    const complaints = await prisma.complaint.findMany();
+    return new Response(JSON.stringify(complaints));
+}
+
+export async function POST(req){
+  const {content, message_id} = await req.json()
+
+  const complaint = await prisma.complaint.create({
+    data:{
+      id: IdManager.complaintId(),
+      content: content,
+      message: {
+        connect:{
+          id: message_id,
+        }
+      },
+    }
+  })
+
+  return Response.json({complaint});
+}
---
+++ app/api/database/messages/route.js
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma";
+import { IdManager } from '@/utils/IdManager'
+
+export async function GET() {
+    const messages = await prisma.message.findMany();
+    return new Response(JSON.stringify(messages));
+}
+
+export async function POST(req){
+  const {session, position, sender, deleted, bookmarked, student_id} = await req.json()
+
+  const message = await prisma.message.create({
+    data:{
+      id: IdManager.messageId(),
+      session: session,
+      position: position,
+      sender: sender,
+      deleted: deleted,
+      bookmarked:bookmarked,
+      student: {
+        connect:{
+          student_id: student_id,
+        }
+      },
+    }
+  })
+
+  return Response.json({message});
+}
