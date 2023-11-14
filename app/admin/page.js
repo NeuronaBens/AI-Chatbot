00519a13ac936fe1d4692b7adcfea5c463df8f2c
NeuronaBens <neuronabens@gmail.com>
2023-11-14 01:38:27 -0500

    feat: stress levels table
    added the stress levels table front-end functionality.

--- app/admin/page.js
+++ app/admin/page.js
@@ -2,14 +2,24 @@
 
 import UserTable from "@/components/admin/dashboards/user-table";
 import AnxietyLevelsTable from "@/components/admin/dashboards/anxiety-levels-table";
+import StressLevelsTable from "@/components/admin/dashboards/stress-levels-table";
 
 const AdminHome = () => {
-  return (
+  const real = (
     <div>
       <UserTable></UserTable>
       <AnxietyLevelsTable></AnxietyLevelsTable>
+      <StressLevelsTable></StressLevelsTable>
     </div>
   );
+
+  const test = (
+    <div>
+      <StressLevelsTable></StressLevelsTable>
+    </div>
+  );
+
+  return test;
 };
 
 export default AdminHome;
