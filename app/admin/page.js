ba86d54d5c3df2def6debcdcad777148eb2787e0
NeuronaBens <neuronabens@gmail.com>
2023-11-14 02:15:17 -0500

    feat: message table

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,6 +4,7 @@ import UserTable from "@/components/admin/dashboards/user-table";
 import AnxietyLevelsTable from "@/components/admin/dashboards/anxiety-levels-table";
 import StressLevelsTable from "@/components/admin/dashboards/stress-levels-table";
 import ComplaintTable from "@/components/admin/dashboards/complaints-table";
+import MessageTable from "@/components/admin/dashboards/messages-table";
 
 const AdminHome = () => {
   const real = (
@@ -12,16 +13,17 @@ const AdminHome = () => {
       <AnxietyLevelsTable></AnxietyLevelsTable>
       <StressLevelsTable></StressLevelsTable>
       <ComplaintTable></ComplaintTable>
+      <MessageTable></MessageTable>
     </div>
   );
 
   const test = (
     <div>
-      <ComplaintTable></ComplaintTable>
+      <MessageTable></MessageTable>
     </div>
   );
 
-  return real;
+  return test;
 };
 
 export default AdminHome;
