b15cffc07d996aeee2556a9eb2a0a37ad0184a0a
NeuronaBens <neuronabens@gmail.com>
2023-11-14 13:03:07 -0500

    feat: notification dashboard

--- app/admin/notificaciones/page.js
+++ app/admin/notificaciones/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NotificacionesForm from "@/components/admin/notifications/page";
+import NotificationTable from "@/components/admin/dashboards/notification-table";
 
 const NotificationPage = () => {
   return (
     <div>
       <NotificacionesForm></NotificacionesForm>
+      <NotificationTable></NotificationTable>
     </div>
   );
 };
