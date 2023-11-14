f4ef06e444c3b50ed57ca36484c947fc3444ba17
NeuronaBens <neuronabens@gmail.com>
2023-11-14 01:31:10 -0500

    feat: anxiety levels table
    added the anxiety levels table front end functionality

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import UserTable from "@/components/admin/dashboards/user-table";
+import AnxietyLevelsTable from "@/components/admin/dashboards/anxiety-levels-table";
 
 const AdminHome = () => {
   return (
     <div>
       <UserTable></UserTable>
+      <AnxietyLevelsTable></AnxietyLevelsTable>
     </div>
   );
 };
