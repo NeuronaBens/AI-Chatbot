fe2ee6132aee555ed098e62c6dcd4cc428e89ee8
Alejandro Silva <u201815776@upc.edu.pe>
2023-11-16 08:05:58 -0500

    feat: FAQs section
    added a FAQs section for the students

---
+++ app/user/ayuda/page.js
@@ -0,0 +1,124 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from '@chakra-ui/react'
+
+export default function FAQs(){
+
+  return(
+    <div>
+      <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white py-6">Preguntas Frecuentes</h1>
+      <Accordion allowToggle  className="mr-10 space-y-8">
+
+        <AccordionItem >
+          <h2>
+            <AccordionButton>
+            <Box as="span" flex='1' textAlign='left'>
+              ¿Como funciona Camlbot?
+            </Box>
+            <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                ¿Qués preguntas le puedo hacer a Calmbot?
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+            <Box as="span" flex='1' textAlign='left'>
+              ¿Como funciona Camlbot?
+            </Box>
+            <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                ¿Qués preguntas le puedo hacer a Calmbot?
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+            <Box as="span" flex='1' textAlign='left'>
+              ¿Como funciona Camlbot?
+            </Box>
+            <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                ¿Qués preguntas le puedo hacer a Calmbot?
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+
+      </Accordion>
+    </div>
+  )
+}
