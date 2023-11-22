4b769a44fedf4e234a95a27c23a066b78d0252c4
NeuronaBens <neuronabens@gmail.com>
2023-11-22 15:25:33 -0500

    feat: unfinished theme change
    check this document.body.classList.add("bg-slate-800");
    and this console.log(session.user.id); /////test on config-dialog.js

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <Providers>
-      <body>{children}</body>
+        <body>{children}</body>
       </Providers>
     </html>
   );
--- app/user/page.js
+++ app/user/page.js
@@ -1,12 +1,13 @@
 import ChatContainer from "@/components/user/chat/chat-container";
-import { getServerSession } from "next-auth/next"
+import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
-  return(
-  <div>
-    <ChatContainer {...session}></ChatContainer>
-  </div>);
+  return (
+    <div>
+      <ChatContainer {...session}></ChatContainer>
+    </div>
+  );
 }
