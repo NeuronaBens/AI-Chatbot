168a74fa97b7948363fd9e0811a35c0a7efeb902
NeuronaBens <neuronabens@gmail.com>
2024-01-25 15:48:00 -0500

    hotfix: used className convention
    Applied the className convention to components.

--- components/general/config-dialog.js
+++ components/general/config-dialog.js
@@ -285,15 +285,15 @@ export default function ConfigDialog({ title, onClose, showDialog }) {
                             {" "}
                             Recopilación de Datos{" "}
                           </label>
-                          <label class="relative inline-flex items-center cursor-pointer">
+                          <label className="relative inline-flex items-center cursor-pointer">
                             <input
                               id="data"
                               type="checkbox"
-                              class="sr-only peer"
+                              className="sr-only peer"
                               onChange={() => handleUpdate("data")}
                               checked={data}
                             />
-                            <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                            <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
                           </label>
                         </div>
                         <div className="space-y-4 flex flex-row justify-between">
--- components/user/chat/textBubbles/options-menu.js
+++ components/user/chat/textBubbles/options-menu.js
@@ -2,30 +2,30 @@ import React from "react";
 
 const OptionsMenu = ({ handleOptionClick }) => {
   return (
-    <div class="absolute z-10 mt-2 w-48 rounded-md shadow-lg">
-      <div class="rounded-md bg-white shadow-xs">
+    <div className="absolute z-10 mt-2 w-48 rounded-md shadow-lg">
+      <div className="rounded-md bg-white shadow-xs">
         <div
-          class="py-1"
+          className="py-1"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="options-menu"
         >
           <button
-            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
             role="menuitem"
             onClick={() => handleOptionClick("Eliminar")}
           >
             Eliminar
           </button>
           <button
-            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
             role="menuitem"
             onClick={() => handleOptionClick("Guardar")}
           >
             Guardar
           </button>
           <button
-            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left"
             role="menuitem"
             onClick={() => handleOptionClick("Denunciar")}
           >
