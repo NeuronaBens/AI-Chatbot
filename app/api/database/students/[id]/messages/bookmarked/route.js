51bd7bf823f06283f6e8e4ef6486716381c81599
NeuronaBens <neuronabens@gmail.com>
2024-03-14 13:09:31 -0500

    Update route.js

--- app/api/database/students/[id]/messages/bookmarked/route.js
+++ app/api/database/students/[id]/messages/bookmarked/route.js
@@ -1,17 +1,19 @@
 import { prisma } from "@/lib/prisma";
 
-export async function GET(req, { params }){
-  const id = params.id
+//directory location: app/api/database/students/[id]/messages/bookmarked/route.js
+
+export async function GET(req, { params }) {
+  const id = params.id;
 
   const message = await prisma.message.findMany({
     where: {
       student_id: id,
-      bookmarked: true
+      bookmarked: true,
     },
     orderBy: {
-      position: 'asc'
-    }
-  })
+      position: "asc",
+    },
+  });
 
-  return Response.json(message)
+  return Response.json(message);
 }
