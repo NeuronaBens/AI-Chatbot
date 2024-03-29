d5c8d6804d453e5ec921f7a5c70c9ec994a0caae
NeuronaBens <neuronabens@gmail.com>
2024-03-29 17:59:26 -0500

    feat: completed api to reset tasks

--- app/api/database/students/[id]/weekly-reset-tasks/route.js
+++ app/api/database/students/[id]/weekly-reset-tasks/route.js
@@ -1,14 +1,24 @@
 import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
-export async function POST(req, { params }) {
+//relative path: app\api\database\students\[id]\weekly-reset-tasks\route.js
+
+export async function DELETE(req, { params }) {
   const id = params.id;
 
-  // Fetch the last message from the database for the given student
-  const tasks = await prisma.studentTask.findMany({
-    where: { student_id: id },
-    orderBy: { date_send: "asc" },
+  // Calculate the date 7 days ago
+  const sevenDaysAgo = new Date();
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+  // Delete student tasks that are more than 7 days old
+  const deleted = await prisma.studentTask.deleteMany({
+    where: {
+      student_id: id,
+      creation_date: {
+        lte: sevenDaysAgo,
+      },
+    },
   });
 
-  //erase any case of 1 week old.
-  //...
+  return NextResponse.json(deleted);
 }
