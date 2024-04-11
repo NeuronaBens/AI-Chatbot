fe88901b0fc2bd828915c9ab7e073719a6157999
NeuronaBens <neuronabens@gmail.com>
2024-04-11 17:07:15 -0500

    feat: is risky endpoint

---
+++ app/api/database/messages/is-risky/route.js
@@ -0,0 +1,18 @@
+// route: app\api\database\messages\is-risky\route.js
+
+export async function POST(req) {
+  const { text } = await req.json();
+
+  // Define the list of sensitive words to check for
+  const sensitiveWords = ["morir", "matar", "suicidar", "suicidio"];
+
+  // Check if the message contains any sensitive words
+  const containsSensitiveWords = sensitiveWords.some((word) =>
+    text.toLowerCase().includes(word)
+  );
+
+  if (containsSensitiveWords) {
+    return Response.json(true);
+  }
+  return Response.json(false);
+}
