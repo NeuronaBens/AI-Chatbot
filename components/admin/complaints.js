fef951c37b8067f6b082e53277fc101813503103
Alejandro Silva <u201815776@upc.edu.pe>
2024-04-12 22:09:50 -0500

    fix: fixed constant complaints reloading

--- components/admin/complaints.js
+++ components/admin/complaints.js
@@ -7,7 +7,6 @@ import TokenBox from "../general/tokenbox";
 
 const Complaints = () => {
   const { data: session, status } = useSession();
-  const [isLoading, setIsLoading] = useState(true);
   const [options, setOptions] = useState([]);
   const [complaints, setComplaints] = useState([]);
   const [initialDate, setInitialDate] = useState(null);
@@ -16,7 +15,6 @@ const Complaints = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true);
       if (session && session.user) {
         const res = await fetch("/api/database/students", {
           method: "GET",
@@ -27,7 +25,6 @@ const Complaints = () => {
         const data = await res.json();
         const user_emails = data.map((student) => student.user.email);
         setOptions(user_emails);
-        setIsLoading(false);
       }
     };
     fetchData();
@@ -67,50 +64,50 @@ const Complaints = () => {
       <br></br>
       <hr className="m-5"></hr>
       <h2 className="text-2xl font-bold mb-4 text-center">Complaints</h2>
-      {!isLoading && (
-        <form
-          className=" rounded-md shadow-xl bg-[#E0DFFF] p-4 mx-4 space-y-2"
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-row space-x-8">
-            <div className="flex flex-col">
-              <label htmlFor="initialDate"> Fecha Inicial: </label>
-              <DatePicker
-                id="initialDate"
-                name="initialDate"
-                selected={initialDate}
-                onChange={(date) => setInitialDate(date)}
-                className=" rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
-              />
-            </div>
 
-            <div className="flex flex-col">
-              <label htmlFor="finalDate"> Fecha Final: </label>
-              <DatePicker
-                id="finalDate"
-                name="finalDate"
-                selected={finalDate}
-                onChange={(date) => setFinalDate(date)}
-                className=" rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
-              />
-            </div>
+      <form
+        className=" rounded-md shadow-xl bg-[#E0DFFF] p-4 mx-4 space-y-2"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex flex-row space-x-8">
+          <div className="flex flex-col">
+            <label htmlFor="initialDate"> Fecha Inicial: </label>
+            <DatePicker
+              id="initialDate"
+              name="initialDate"
+              selected={initialDate}
+              onChange={(date) => setInitialDate(date)}
+              className=" rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
+            />
           </div>
 
-          <div>
-            <label htmlFor="emails"> Usuarios seleccionados: </label>
-            <TokenBox options={options} onChange={handleTokenChange}></TokenBox>
+          <div className="flex flex-col">
+            <label htmlFor="finalDate"> Fecha Final: </label>
+            <DatePicker
+              id="finalDate"
+              name="finalDate"
+              selected={finalDate}
+              onChange={(date) => setFinalDate(date)}
+              className=" rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10"
+            />
           </div>
+        </div>
 
-          <div className="flex justify-center items-center py-4">
-            <button
-              type="submit"
-              className={`group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
-            >
-              Buscar
-            </button>
-          </div>
-        </form>
-      )}
+        <div>
+          <label htmlFor="emails"> Usuarios seleccionados: </label>
+          <TokenBox options={options} onChange={handleTokenChange}></TokenBox>
+        </div>
+
+        <div className="flex justify-center items-center py-4">
+          <button
+            type="submit"
+            className={`group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+          >
+            Buscar
+          </button>
+        </div>
+      </form>
+
       <div className="relative wrap overflow-hidden p-10 h-full">
         {complaints.length > 0 && (
           <div className="border-2-2 absolute border-[#363447] h-full border-2 left-1/2"></div>
