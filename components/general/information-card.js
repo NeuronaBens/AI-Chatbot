05932accd8563c5644499251f62f3b8c945c92f5
Alejandro Silva <u201815776@upc.edu.pe>
2024-05-07 04:22:05 -0500

    fix: responsive landing

--- components/general/information-card.js
+++ components/general/information-card.js
@@ -1,25 +1,26 @@
-// components/information-card.jsx
 import React from "react";
 import Image from "next/image";
 
-const InformationCard = ({ image, title, description }) => (
-  <div className="relative rounded-lg shadow-xl overflow-hidden">
-    <Image
-      src={image}
-      alt={title}
-      width={600}
-      height={400}
-      className="w-full h-auto"
-    />
-    <div className="absolute bottom-0 left-0 right-0 p-6 bg-gradient-to-t from-black to-transparent">
-      <h3 className="text-xl md:text-2xl font-semibold mb-2 text-white">
-        {title}
-      </h3>
-      <p className="text-sm md:text-base text-white leading-relaxed">
-        {description}
-      </p>
+const InformationCard = ({ image, title, description }) => {
+  return (
+    <div className="rounded-lg shadow-lg overflow-hidden relative">
+      <Image
+        src={image}
+        alt={title}
+        width={600}
+        height={400}
+        className="w-full h-auto"
+      />
+      <div className="absolute inset-0 flex items-end">
+        <div className="py-1 px-2 md:p-6 rounded-lg text-white md:w-full bg-gradient-to-t from-black to-transparent">
+          <h3 className="text-base md:text-xl font-semibold mb-1 md:mb-2">
+            {title}
+          </h3>
+          <p className="text-sm md:text-base">{description}</p>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default InformationCard;
