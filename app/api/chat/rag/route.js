ed293df2e4b3fb6ef9ddfdd5f4d11fd2c0de9c64
NeuronaBens <neuronabens@gmail.com>
2024-05-16 22:30:53 -0500

    Create route.js

---
+++ app/api/chat/rag/route.js
@@ -0,0 +1,4 @@
+//update the prisma schema so that it includes the new table
+//it seems that using a table with a vector as json, does not work, because there is no easy way
+//calculating the closest vector on that form, you will need to use (option a) vector data type
+//or use the (possibly existant) (option b) native functionality to create vector embedding on supabase
